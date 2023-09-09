9e78268877533d1b4c549b22b642f31f1c8597ce
Ricardo Elena <ricardoelena@gmail.com>
2023-09-09 16:38:09 -0400

    Update login.sh

--- login.sh
+++ login.sh
@@ -18,17 +18,24 @@ session_check () {
 }
 
 
-#checks the script comes with one argument
+get_IdP () {
+    curl -k --location --request GET $base/identity-providers/names \
+        --header 'Content-Type: application/json' \
+        --header 'Accept: application/vnd.appgate.peer-v17+json' | jq 
+}
+
+#checks that there is at least  one argument
 if [ $# -eq 0 ]; then
 	echo "No arguments provided"
-	command_help
-	exit 0 
+	#command_help
+	#exit 0 
 fi
 
 #initialize vars
 server="" #parameter expected as arg
 auth_ok=""
 base=""
+idp=""
 #base="https://$server:8443/admin" #base expects port 8443
 deviceid=`uuidgen | tr "[:upper:]" "[:lower:]"` #every login a random uuid will be generated
 token="" #token placeholder
@@ -52,7 +59,7 @@ fi
 #Get arguments
 while getopts 'u:p:s:f:h' opt; do
 	case "${opt}" in
-    		u)	user="$OPTARG" ;;
+    	u)	user="$OPTARG" ;;
 		p)	pass="$OPTARG" ;;
 		s) 
 			server="${OPTARG}"
@@ -65,6 +72,7 @@ while getopts 'u:p:s:f:h' opt; do
 			fi
 			user=`cat $OPTARG | grep user | cut -d= -f2`
 			pass=`cat $OPTARG | grep pass | cut -d= -f2`
+			idp=`cat $OPTARG | grep idp | cut -d= -f2`
 		;;
 		h)
 			command_help
@@ -80,11 +88,22 @@ done
 #Validate required values
 if [ -z "$server" ]
 then
-	errorlog="-No server provided\n"
+    echo -n Controller FQDN:
+    read server
+    base="https://$server:8443/admin"
+	#errorlog="-No server provided\n"
 fi
 if [ -z "$user" ]
 then
-	errorlog="${errorlog}-No user provided\n"
+    echo $server
+    get_IdP > data/$server.idp   
+    echo "Select the IdP"
+    cat data/$server.idp | jq -r '.data[] | "\(.name)"' | awk '{print NR, $0}'
+    read idp 
+    idp=`cat data/$server.idp | jq -r --arg jqvar "$idp" '.data[($jqvar | tonumber) - 1].name' `
+	echo -n User:
+    read user
+    #errorlog="${errorlog}-No user provided\n"
 fi
 if [ -z "$pass" ]
 then
@@ -95,7 +114,9 @@ then
 
 	if [ -z "$pass" ]
 	then
-		errorlog="${errorlog}-No password provided\n"
+        echo -n Password:
+        read -s pass
+	#	errorlog="${errorlog}-No password provided\n"
 	fi
 fi
 
@@ -107,22 +128,23 @@ fi
 
 #Funtion in charge of auth token request and establish expiration date for token information save under "data/$server"
 login () { 
+    get_IdP 
 	curl -k --location --request POST $base/login \
 	--header 'Content-Type: application/json' \
-	--header 'Accept: application/vnd.appgate.peer-v15+json' \
+	--header 'Accept: application/vnd.appgate.peer-v17+json' \
 	--data-raw '{
-	    "providerName": "local",
+	    "providerName": "'"$idp"'",
 	    "deviceId": "'"$deviceid"'",
 	    "username": "'"$user"'",
 	    "password": "'"$pass"'",
 	    "samlResponse": "" 
-	}'  | python -m json.tool 
+	}'  | jq
 }
 
 #funtion in charge of dowload pem cert from controller
 cert () { 
 	curl -k --location --request GET $base/certificate-authority/ca \
-	--header 'Accept: application/vnd.appgate.peer-v15+json' | python -m json.tool > data/$server.cert
+	--header 'Accept: application/vnd.appgate.peer-v17+json' | jq > data/$server.cert
 	echo -----BEGIN CERTIFICATE----- > cert/$server.pem
 	jq '.certificate' data/$server.cert | tr -d '"' >> cert/$server.pem
 	echo -----END CERTIFICATE----- >> cert/$server.pem
